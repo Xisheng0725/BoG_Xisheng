9a7562df04f382d573bc1ff10783f28e88f4c8ed
zhangxisheng <xzhang3033@gatech.edu>
2022-10-16 22:54:48 -0500

    add name

--- Mid-project/pok.js
+++ Mid-project/pok.js
@@ -1,15 +1,29 @@
 let id = 1;
-var right = document.getElementById("buttonRight");
-var left = document.getElementById("buttonleft");
-const onRightButtonClick = () => {
-    id++;
-    refreshImg(id);
-}
+
 const refreshImg = (id) => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
         .then((resp) => resp.json())
         .then((data) => {
             var image = document.getElementById("image");
             image.src = data.sprites.front_default;
+            var name = document.getElementById("name");
+            name.rows[0].cells[0].innerHTML = data.species.name;
         });
+}
+
+var right = document.getElementById("buttonRight");
+var left = document.getElementById("buttonleft");
+const onRightButtonClick = (right) => {
+    if (id === 904) {
+        return;
+    }
+    id++;
+    refreshImg(id);
+}
+const onLeftButtonClick = (left) => {
+    if(id === 1) {
+        return;
+    }
+    id--;
+    refreshImg(id);
 }
